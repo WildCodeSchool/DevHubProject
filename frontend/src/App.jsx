93a6f07f5d43ac2d78c02b867cf31bdaa497dd02
Eleven <95743491+RolandEsnay@users.noreply.github.com>
2023-02-22 14:26:38 +0100

    added MUI theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 import Home from "./pages/Home/Home";
 import AddProject from "./pages/AddProject/AddProject";
 import Calendar from "./pages/Calendar/Calendar";
 import Contact from "./pages/Contact/Contact";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import Dashboard from "./pages/Dashboard/Index";
 import JoinProject from "./pages/JoinProject/JoinProject";
 import Login from "./pages/Login/Login";
 import Mailbox from "./pages/Mailbox/Mailbox";
@@ -15,25 +17,33 @@ import NotFound from "./pages/NotFound/NotFound";
 import "./App.css";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="add-project" element={<AddProject />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/join-project" element={<JoinProject />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/mailbox" element={<Mailbox />} />
-          <Route path="/userprofile/:id" element={<UserProfile />} />
-          <Route path="/roadmap" element={<Roadmap />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+            <BrowserRouter>
+              <Routes>
+                <Route path="*" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+                <Route path="add-project" element={<AddProject />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/join-project" element={<JoinProject />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/mailbox" element={<Mailbox />} />
+                <Route path="/userprofile/:id" element={<UserProfile />} />
+                <Route path="/roadmap" element={<Roadmap />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </BrowserRouter>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
